c48aae6c763e807478ee103aa260f1ba879d6e42
hij1nx <fernmicro@gmail.com>
2011-09-29 11:45:42 -0400

    first commit

---
+++ test/dummy.js
@@ -0,0 +1,54 @@
+
+var test1 = 10 + 10;
+
+module.exports.test2 = function(a, b) {
+  return a + b;
+};
+
+var NODEJITSU = { A: {}, B: {} }
+
+NODEJITSU.A = function(a, b) {
+
+  var na = 'nested';
+  
+  return a + b;
+};
+
+NODEJITSU.B = function(a, b) {
+
+  var nb = 'nested';
+  
+  return a + b;
+};
+
+
+test3 = 100;
+test4 = { test4A: 1 };
+
+function test5() {
+  return true;
+};
+
+var test6 = function() {
+  return String(Date.now())
+};
+
+var test7 = function() {
+  
+}();
+
+var test8 = function test7() {
+  
+}();
+
+var test9 = (function() {
+  
+})();
+
+var test10 = (function() {
+  
+}());
+
+var test11 = {
+  foo: function test9() {}
+};
---
+++ test/extraction-test.js
@@ -0,0 +1,108 @@
+var extraction = require('nodeunit').testCase;
+var Codesurgeon = require('../lib/filesurgeon').Codesurgeon;
+var vm = require('vm');
+
+module.exports = extraction({
+
+  setUp: function (callback) {
+    callback();
+  },
+
+  tearDown: function (callback) {
+    callback();
+  },
+
+  '1. Extract a method by `simple` name.': function (test) {
+    var surgeon = new Codesurgeon;
+
+    var sandbox = {};
+
+    surgeon
+
+      //
+      // if we want to see logging information or not.
+      //
+      .configure({ quiet: true })
+
+      //
+      // read one, or a couple of files
+      //
+      .read(__dirname + '/dummy.js')
+
+      //
+      // get one or more methods from the code that we've read in.
+      //
+      .extract('test5');
+
+    vm.runInNewContext(surgeon.output, sandbox, 'sandbox.vm');
+    
+    test.ok(sandbox.test5(), 'The function was extracted and executed.')
+    
+    test.expect(1);
+    test.done();
+  },
+  
+  '2. Extract a method by dot notation.': function (test) {
+    var surgeon = new Codesurgeon;
+
+    var sandbox = {};
+
+    surgeon
+
+      //
+      // if we want to see logging information or not.
+      //
+      .configure({ quiet: true })
+
+      //
+      // read one, or a couple of files
+      //
+      .read(__dirname + '/dummy.js')
+
+      //
+      // get one or more methods from the code that we've read in.
+      //
+      .extract('NODEJITSU.B');
+
+    vm.runInNewContext(surgeon.output, sandbox, 'sandbox.vm');
+
+    test.ok(sandbox.NODEJITSU.B, 'The function was extracted and executed.')
+
+    test.expect(1);
+    test.done();
+  },
+
+
+  '2. Add a single listener on a single event.': function (test) {
+    var surgeon = new Codesurgeon;
+
+    var sandbox = {};
+
+    surgeon
+
+      //
+      // if we want to see logging information or not.
+      //
+      .configure({ quiet: true })
+
+      //
+      // read one, or a couple of files
+      //
+      .read(__dirname + '/dummy.js')
+
+      //
+      // get one or more methods from the code that we've read in.
+      //
+      .extract('test5');
+
+    vm.runInNewContext(surgeon.output, sandbox, 'sandbox.vm');
+    
+    test.ok(sandbox.test5(), 'The function was extracted and executed.')
+    
+    test.expect(1);
+    test.done();
+  }
+
+
+});
+
