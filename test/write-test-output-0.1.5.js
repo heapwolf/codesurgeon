5497eefa232277f2ff5e04e43f00630a7f853f35
hij1nx <hij1nx@me.com>
2011-10-02 05:35:30 +0200

    [lib] output file naming issue resuloved

---
+++ test/write-test-output-0.1.5.js
@@ -0,0 +1,25 @@
+
+
+//
+// Generated on Sun Oct 02 2011 05:34:57 GMT+0200 (CEST) by Nodejitsu Inc <info@nodejitsu.com>
+// Version 0.1.5
+//
+function test5() {
+    return true;
+}
+
+//
+// Generated on Sun Oct 02 2011 05:34:57 GMT+0200 (CEST) by Nodejitsu Inc <info@nodejitsu.com>
+// Version 0.1.5
+//
+var test6 = function() {
+    return String(Date.now());
+};
+
+//
+// Generated on Sun Oct 02 2011 05:34:57 GMT+0200 (CEST) by Nodejitsu Inc <info@nodejitsu.com>
+// Version 0.1.5
+//
+var test10 = function() {
+    return "test10";
+}();
---
+++ test/write-test-output-wrap-0.1.5.js
@@ -0,0 +1,12 @@
+
+
+//
+// Generated on Sun Oct 02 2011 05:34:57 GMT+0200 (CEST) by Nodejitsu Inc <info@nodejitsu.com>
+// Version 0.1.5
+//
+
+(function(exports) {
+
+exports.hello = "Hello, World.";
+
+}(window));
